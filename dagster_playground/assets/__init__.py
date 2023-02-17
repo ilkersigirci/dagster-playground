f75f73bfafd2ae734e518119651cdd5fe7db4bc8
ilker.sigirci <ilker.sigirci@data-boss.com.tr>
2023-02-17 14:12:21 +0300

    asset file renamed

--- dagster_playground/assets/__init__.py
+++ dagster_playground/assets/__init__.py
@@ -3,9 +3,9 @@ from dagster import load_assets_from_package_module
 from . import temporary, tutorial
 
 temporary_assets = load_assets_from_package_module(
-    package_module=temporary, key_prefix="test", group_name="TemporaryGroup"
+    package_module=temporary, key_prefix="temp", group_name="TemporaryGroup"
 )
 
 tutorial_assets = load_assets_from_package_module(
-    package_module=tutorial, key_prefix="test", group_name="TutorialGroup"
+    package_module=tutorial, key_prefix="tut", group_name="TutorialGroup"
 )
