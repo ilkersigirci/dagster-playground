1ad31db7af67615ac0294a9d3daf5c2a2ce273ef
ilker.sigirci <ilker.sigirci@data-boss.com.tr>
2023-02-22 11:32:14 +0300

    multi assets && asset graphs added

--- dagster_playground/assets/tutorial/from_graph.py
+++ dagster_playground/assets/tutorial/from_graph.py
@@ -1,13 +1,18 @@
-from typing import Tuple
-
 from dagster import (
-    AssetOut,
+    AssetKey,
     AssetsDefinition,
     GraphOut,
+    OpExecutionContext,
+    Out,
+    Output,
     asset,
     graph,
-    multi_asset,
     op,
+)
+
+from dagster_playground.assets.tutorial.multi_assets import (
+    simple_multi_asset,
+    simple_multi_asset_with_upstream,
 )
 
 
@@ -19,13 +24,6 @@ def add_three(num: int) -> int:
 @asset
 def upstream_asset() -> int:
     return 1
-
-
-@multi_asset(
-    outs={"int_output_multi_asset": AssetOut(), "str_output_multi_asset": AssetOut()}
-)
-def two_output_asset(upstream_asset: int) -> Tuple[int, str]:
-    return (upstream_asset + 5, "foo")
 
 
 @graph
@@ -46,11 +44,73 @@ def downstream_asset(middle_asset_graph):
 
 
 @graph(out={"first_asset_graph": GraphOut(), "second_asset_graph": GraphOut()})
-def two_assets_graph(upstream_asset):
-    one, two = two_output_asset(upstream_asset)
-    one, two = two_output_asset(one)
+def two_assets_graph():
+    one, two = simple_multi_asset()
 
     return {"first_asset_graph": one, "second_asset_graph": two}
 
 
-two_assets = AssetsDefinition.from_graph(two_assets_graph)
+@graph(
+    out={
+        "first_asset_graph_upstream": GraphOut(),
+        "second_asset_graph_upstream": GraphOut(),
+    }
+)
+def two_assets_graph_upstream(upstream_asset: int):
+    one, two = simple_multi_asset_with_upstream(upstream_asset)
+
+    return {"first_asset_graph_upstream": one, "second_asset_graph_upstream": two}
+
+
+two_assets_graph_asset = AssetsDefinition.from_graph(two_assets_graph)
+two_assets_graph_upstream = AssetsDefinition.from_graph(two_assets_graph_upstream)
+
+#############################################################################
+
+
+@graph(out={"one": GraphOut(), "two": GraphOut()})
+def return_one_and_two(zero):
+    one, two = simple_multi_asset_with_upstream(zero)
+
+    return {"one": one, "two": two}
+
+
+explicit_deps_graph_asset = AssetsDefinition.from_graph(
+    return_one_and_two,
+    keys_by_input_name={"zero": AssetKey("upstream_asset")},
+    keys_by_output_name={
+        "one": AssetKey("asset_one"),
+        "two": AssetKey("asset_two"),
+    },
+)
+
+#############################################################################
+
+
+@op(out={"foo_1": Out(is_required=False), "foo_2": Out(is_required=False)})
+def subset_op(context: OpExecutionContext, bar_1):
+    # Selectively returns outputs based on selected assets
+    if "foo_1" in context.selected_output_names:
+        yield Output(bar_1 + 1, output_name="foo_1")
+    if "foo_2" in context.selected_output_names:
+        yield Output(bar_1 + 2, output_name="foo_2")
+
+
+@op(out={"bar_1": Out(), "bar_2": Out()})
+def bar():
+    return 1, 2
+
+
+@op
+def baz(foo_2, bar_2):
+    return foo_2 + bar_2
+
+
+@graph(out={"foo_asset": GraphOut(), "baz_asset": GraphOut()})
+def subset_graph():
+    bar_1, bar_2 = bar()
+    foo_1, foo_2 = subset_op(bar_1)
+    return {"foo_asset": foo_1, "baz_asset": baz(foo_2, bar_2)}
+
+
+subset_graph = AssetsDefinition.from_graph(subset_graph, can_subset=True)
---
+++ dagster_playground/assets/tutorial/multi_assets.py
@@ -0,0 +1,53 @@
+from typing import Tuple
+
+from dagster import AssetKey, AssetOut, OpExecutionContext, Output, multi_asset
+
+
+@multi_asset(
+    outs={
+        "simple_int_multi_asset": AssetOut(),
+        "simple_str_multi_asset": AssetOut(),
+    }
+)
+def simple_multi_asset() -> Tuple[int, str]:
+    return 123, "abc"
+
+
+# TODO: Remove later since it is redunda.
+@multi_asset(
+    outs={
+        "simple_int_multi_asset_upstream": AssetOut(),
+        "simple_str_multi_asset_upstream": AssetOut(),
+    }
+)
+def simple_multi_asset_with_upstream(upstream_asset: int) -> Tuple[int, str]:
+    return (upstream_asset, "abc")
+
+
+@multi_asset(
+    outs={
+        "a": AssetOut(is_required=False),
+        "b": AssetOut(is_required=False),
+    },
+    can_subset=True,
+)
+def split_actions(context: OpExecutionContext):
+    if "a" in context.selected_output_names:
+        yield Output(value=123, output_name="a")
+    if "b" in context.selected_output_names:
+        yield Output(value=456, output_name="b")
+
+
+@multi_asset(
+    outs={"c": AssetOut(), "d": AssetOut()},
+    internal_asset_deps={
+        "c": {AssetKey("a")},
+        "d": {AssetKey("b")},
+    },
+)
+def subset_deps_multi_asset(a, b):
+    # c only depends on a
+    yield Output(value=a + 1, output_name="c")
+
+    # d only depends on b
+    yield Output(value=b + 1, output_name="d")
