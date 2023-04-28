9bd851c45e7e9042d539c729fb06ad1df3c09a08
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2023-04-28 17:08:45 +0300

    pythonic config None tests

--- dagster_playground/assets/tutorial/pythonic_config.py
+++ dagster_playground/assets/tutorial/pythonic_config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from dagster import Config, RunConfig, asset, materialize_to_memory
 from pydantic import Field
 
@@ -5,29 +7,37 @@ from dagster_playground.resources.pythonic_resource import CredentialsResource
 
 
 class MyAssetConfig(Config):
-    name: str = "Ilker"
-    age: int = Field(31, gt=0, lt=100, description="Age of the person")
-    # Here, the ellipses `...` indicates that the field is required and has no default value.
-    job: str = Field(..., description="Job of the person")
+    simple_default_value: str = "default_string"
+    int_validation: int = Field(default=31, gt=0, lt=100, description="Description 0")
+    simple_none: Optional[str] = None
+    none_field: Optional[str] = Field(default=None, description="Description 1")
+    # NOTE: `...` indicates that the field is required and has no default value.
+    required_field: str = Field(default=..., description="Description 2")
+    # NOTE: Specify a field that can take a  None value while still being required
+    required_none_field: Optional[str] = Field(default=..., description="Description 3")
 
 
 @asset
 def pythonic_asset(config: MyAssetConfig) -> str:
-    return f"Hello {config.name}"
+    assert config.simple_none is None
+
+    return f"Hello {config.simple_default_value}"
 
 
 @asset
 def pythonic_asset_with_resource(
     config: MyAssetConfig, cred_store_local: CredentialsResource
 ) -> str:
-    return f"Hello {config.name} with user: {cred_store_local.username}"
+    return f"Hello {config.simple_default_value} with user: {cred_store_local.username}"
 
 
 if __name__ == "__main__":
     my_config = MyAssetConfig(
-        name="Ilker",
-        job="Engineer",
-        age=26,
+        simple_default_value="Default value",
+        int_validation=26,
+        required_field="Required field",
+        # required_none_field=None,  # FIXME: Not working
+        required_none_field="Required None field",
     )
 
     result = materialize_to_memory(
--- dagster_playground/resources/pythonic_resource.py
+++ dagster_playground/resources/pythonic_resource.py
@@ -2,16 +2,5 @@ from dagster import ConfigurableResource, EnvVar  # noqa: F401
 
 
 class CredentialsResource(ConfigurableResource):
-    username: str = "Local username"
-    password: str = "Local password"
-
-
-# @asset
-# def get_current_username(cred_store: CredentialsResource):
-#     return cred_store.username
-
-
-# defs = Definitions(
-#     assets=[get_current_username],
-#     resources={"cred_store": CredentialsResource(username=EnvVar("USERNAME"))},
-# )
+    username: str = "Username"
+    password: str = "Password"
